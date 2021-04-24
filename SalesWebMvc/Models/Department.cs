b5d3e5256bb2d7c65ef66e428f15ac5b7822646d
Seu Nome <dfernandoficial@gmail.com>
2021-04-23 22:39:34 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models
 {
     public class Department
     {
-        public int Id  { get; set; }
-        public string Name  { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Criando o Atributo e com o " = new List<Seller>()" Instanciamos o Atributo
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Seller.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
